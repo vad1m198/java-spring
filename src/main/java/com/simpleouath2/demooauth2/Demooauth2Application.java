be8f02e4dc06bc373e0fa48242838365aacc62ec
vadim merkotan <vadim.merkotan@silverlinecrm.com>
2018-02-17 08:02:29 +0200

    initial commit

---
+++ src/main/java/com/simpleouath2/demooauth2/Demooauth2Application.java
@@ -0,0 +1,40 @@
+package com.simpleouath2.demooauth2;
+
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+@RestController
+public class Demooauth2Application extends WebSecurityConfigurerAdapter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demooauth2Application.class, args);
+	}
+	
+	@RequestMapping("/user")
+    public Principal user(Principal principal) {
+      return principal;
+    }
+	
+	@Override
+	  protected void configure(HttpSecurity http) throws Exception {
+	    http
+	      .antMatcher("/**")
+	      .authorizeRequests()
+	        .antMatchers("/", "/login**", "/webjars/**")
+	        .permitAll()
+	      .anyRequest()
+	        .authenticated()
+	        .and().logout().logoutSuccessUrl("/").permitAll()
+	        .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	  }
+}
---
+++ src/test/java/com/simpleouath2/demooauth2/Demooauth2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.simpleouath2.demooauth2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demooauth2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
