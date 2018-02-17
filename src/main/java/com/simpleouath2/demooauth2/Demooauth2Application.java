54ec3071325b9898d59bc92cf73861f600553638
vadim merkotan <vadim.merkotan@silverlinecrm.com>
2018-02-17 08:39:28 +0200

    show accounts

--- src/main/java/com/simpleouath2/demooauth2/Demooauth2Application.java
+++ src/main/java/com/simpleouath2/demooauth2/Demooauth2Application.java
@@ -1,12 +1,15 @@
 package com.simpleouath2.demooauth2;
 
 import java.security.Principal;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +23,17 @@ public class Demooauth2Application extends WebSecurityConfigurerAdapter {
 		SpringApplication.run(Demooauth2Application.class, args);
 	}
 	
+	@Autowired
+    private Force force;
+	
 	@RequestMapping("/user")
     public Principal user(Principal principal) {
       return principal;
+    }
+
+    @RequestMapping("/accounts")
+    public List<Force.Account> accounts(OAuth2Authentication principal) {    	
+        return force.accounts(principal);
     }
 	
 	@Override
